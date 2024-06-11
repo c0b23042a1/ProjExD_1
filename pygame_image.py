fec48f127b9ec2df7d5a7df2f1a75bed3ef1c14b
c0b23042 <c0b23042a1@edu.teu.ac.jp>
2024-06-11 16:32:40 +0900

    練習７

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_flp = pg.transform.flip(bg_img, True, False)
     kk = pg.image.load("fig/3.png")
     kk = pg.transform.flip(kk, True, False)
     tmr = 0
@@ -17,8 +18,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
             
-        x = tmr%800
+        x = tmr%3200
         screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_flp, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bg_flp, [-x+4800, 0])
         kk_rct = kk.get_rect()      #こうかとんRectの抽出
         kk_rct.center = 300, 200
         screen.blit(kk, kk_rct)     #birdをbird_rctの設定に従って貼付け
