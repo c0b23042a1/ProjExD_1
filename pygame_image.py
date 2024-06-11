c6e252887251dbf38b9c9e36ca107fee9c462980
c0b23042 <c0b23042a1@edu.teu.ac.jp>
2024-06-11 16:20:11 +0900

    練習６

--- pygame_image.py
+++ pygame_image.py
@@ -16,8 +16,9 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
+            
+        x = tmr%800
+        screen.blit(bg_img, [-x, 0])
         kk_rct = kk.get_rect()      #こうかとんRectの抽出
         kk_rct.center = 300, 200
         screen.blit(kk, kk_rct)     #birdをbird_rctの設定に従って貼付け
